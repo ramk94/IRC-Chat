03aa4605e37270a8beafd167c3db6a52d30368f7
ramk94 <32658991+ramk94@users.noreply.github.com>
2018-05-28 18:09:29 -0700

    IRC client

---
+++ client.py
@@ -0,0 +1,38 @@
+import socket               # Import socket module
+import threading
+import sys
+import time
+
+host = socket.gethostname() # Get local machine name
+
+port = 8083                # Reserve a port for your service.
+nicks= []
+class Client:
+
+	soc=socket.socket(socket.AF_INET,socket.SOCK_STREAM)  # Create a socket object
+
+
+	
+	def __init__(self):
+		self.soc.connect((host,port))
+		#continuously run on the background
+		iThread=threading.Thread(target=self.sendMsg)
+		iThread.daemon = True
+		iThread.start()		
+
+		while True:
+			data=self.soc.recv(1024).decode('utf-8')
+			if not data:
+				break
+			print(time.strftime("%H:%M:%S")," ",data)
+
+	def sendMsg(self):
+		alias=input("Nickname: ")
+		message='#NICK '+alias
+		while True:
+			self.soc.send(message.encode('utf-8'))
+			message=input('')
+
+
+chatclient=Client()
+
